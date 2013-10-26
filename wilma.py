9cd57d9847c60782f40544ef8c7175ede9cacd71
kimvais <kimvais@ssh.com>
2013-10-26 23:13:39 +0300

    Added requirements, can fetch all messages to local files.

--- wilma.py
+++ wilma.py
@@ -1,40 +1,54 @@
+import logging
 import bs4
 import requests
 
+logger = logging.getLogger(__name__)
+
 def main(conf, user):
     session = requests.Session()
-    frontpage = bs4.BeautifulSoup(session.get(conf.BASEURL).text)
+    req = session.get(conf.BASEURL)
+    logger.info('{0} - {1}'.format(req.url, req.status_code))
+    frontpage = bs4.BeautifulSoup(req.text)
     session_id = frontpage.find('input', attrs=dict(name='SESSIONID')).attrs['value']
+    logger.debug("session id: {0}".format(session_id))
     logindata = dict(SESSIONID=session_id,
             Login=conf.USERS[user]['username'],
             Password=conf.USERS[user]['password'])
-    _ = session.post(conf.BASEURL + '/login', logindata)
-    messages_html = session.get(conf.BASEURL + '/messages').text
-    messages = bs4.BeautifulSoup(messages_html)
+    req = session.post(conf.BASEURL + '/login', logindata)
+    logger.info('{0} - {1}'.format(req.url, req.status_code))
+    req = session.get(conf.BASEURL + '/messages')
+    logger.info('{0} - {1}'.format(req.url, req.status_code))
+    messages = bs4.BeautifulSoup(req.text)
     for message_link in messages.find_all('a', attrs={'class':'fitt'}):
         message_id = message_link.attrs['href'].rsplit('/',1)[-1]
-        msg_page_html = session.get(conf.BASEURL +
-                '/messages/{0}'.format(message_id)).text
-        msg = bs4.BeautifulSoup(msg_page_html).find('div',
+        req = session.get(conf.BASEURL +
+                '/messages/{0}'.format(message_id))
+        logger.info('{0} - {1}'.format(req.url, req.status_code))
+        msg = bs4.BeautifulSoup(req.text).find('div',
                 attrs={'class':'columns-left-inner'})
         message = '\n\n'.join(x.text for x in msg.find_all('p'))
-        sender, recipient, timestamp = (x.text for x in msg.find_all('td'))
-        sender = sender.replace('\xa0', ' ')
+        try:
+            sender, recipient, timestamp = (x.text for x in msg.find_all('td')[:3])
+        except ValueError:
+            logger.warning(msg.find_all('td'))
 
         try:
             with open('data/{0}_{1}.txt'.format(user, message_id)) as f:
                 pass
         except IOError:
             with open('data/{0}_{1}.txt'.format(user, message_id), 'w') as f:
-                f.write(sender + '\n')
-                f.write(recipient + '\n')
+                f.write(sender.encode('utf-8') + '\n')
+                f.write(recipient.encode('utf-8')+ '\n')
                 f.write(timestamp + '\n')
-                f.write(message + '\n')
+                f.write(message.encode('utf-8') + '\n')
 
     # Log out
     session.post(conf.BASEURL + '/logout', dict(loginbtn="Kirjaudu ulos"))
 
 if __name__ == "__main__":
+    logging.basicConfig()
+    logger.setLevel(logging.DEBUG)
     import settings
+    logger.debug("Calling main")
     main(settings, 'Elias')
 
